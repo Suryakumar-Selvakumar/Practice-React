0b9c9043ea5849c4409547dd7ed5e0298a2ecf6d
Suryakumar Selvakumar <ssuryakumar2077@gmail.com>
2024-12-13 10:39:12 -0700

    feat(`FunctionalInput.jsx`): Create component

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,9 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-function Person() {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
 
-  function handleFirstNameInput(e) {
-    setFirstName(e.target.value);
-  }
-
-  function handleLastNameInput(e) {
-    setLastName(e.target.value);
-  }
-
-  return (
-    <>
-      <label htmlFor="first-name">First Name: </label>
-      <input type="text" id="first-name" onChange={handleFirstNameInput}/>
-      <label htmlFor="last-name">Last Name: </label>
-      <input type="text" id="last-name" onChange={handleLastNameInput}/>
-      <h1>{firstName + ' ' + lastName}</h1>
-    </>
-  );
-}
 
 function App() {
-
   return (
     <div>
       <Person />
