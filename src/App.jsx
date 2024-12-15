3dbf25210c93a4f59e8475bd79abcb2e7bd54393
Suryakumar Selvakumar <ssuryakumar2077@gmail.com>
2024-12-14 20:04:52 -0700

    ci: Install vitest & add script to `package.json`

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
 import "./App.css";
-import { FunctionalInput } from "../components/FunctionalInput";
-import ClassInput from "../components/ClassInput";
-import Count from "../components/Count";
 
 function App() {
   return (
     <>
-      <FunctionalInput name="Functional component" />
-      <div className="divider" />
-      <ClassInput name="Class based component" />
     </>
   );
 }
