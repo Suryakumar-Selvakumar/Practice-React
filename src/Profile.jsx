cd53d7342bb00f1408a33755ca8b2ec90446b50c
Suryakumar Selvakumar <ssuryakumar2077@gmail.com>
2024-12-21 15:30:49 -0700

    feat: Get used to working with react-router
    Learn to use
        - routes with Link comps,
        - nested routes by setting children in the router config,
        - outlets to render child comps alongside the parent,
        - default page by setting index to true,
        - dynamic segments using useParams hook,
        - an errorElement to handle bad urls,
        - routes config across multiple files

---
+++ src/Profile.jsx
@@ -0,0 +1,27 @@
+import { Outlet } from "react-router-dom";
+import Spinach from "./Spinach.jsx";
+import Popeye from "./Popeye.jsx";
+import DefaultProfile from "./DefaultProfile.jsx";
+import { useParams } from "react-router-dom";
+
+const Profile = () => {
+  const { name } = useParams();
+
+  return (
+    <div>
+      <h1>Hello from profile page!</h1>
+      <p>So, how are you?</p>
+      <hr />
+      <h2>The profile visited is here:</h2>
+      {name === "popeye" ? (
+        <Popeye />
+      ) : name === "spinach" ? (
+        <Spinach />
+      ) : (
+        <DefaultProfile />
+      )}
+    </div>
+  );
+};
+
+export default Profile;
